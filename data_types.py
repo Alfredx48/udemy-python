3f5295c9adbbb796b9181c9a28b1476320e08ed9
Alfred Shaheen <shaheenalfred48@gmail.com>
2024-01-22 14:30:26 -0800

    started new udemy course

---
+++ data_types.py
@@ -0,0 +1,27 @@
+#Fundamental Data Types
+int 
+print(type(2 + 4))
+float
+print(type(2 / 4))
+bool
+print(type(False))
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized Data Types
+
+print((5 + 4) * 10 / 2)
+
+print(((5 + 4) * 10) / 2)
+
+print((5 + 4) * (10 / 2))
+
+print(5 + (4 * 10) / 2)
+
+print(5 + 4 * 10 // 2)
+None
